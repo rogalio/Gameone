ff955b093540d694f240c9774455a37070d18a40
Rogalio mouchel <you@example.com>
2022-04-27 19:26:24 +0200

    hello

--- GameOn-website-FR/starterOnly/modal.js
+++ GameOn-website-FR/starterOnly/modal.js
@@ -14,6 +14,17 @@ const formData = document.querySelectorAll(".formData");
 //mydom
 const closed = document.querySelectorAll(".close");
 const first = document.getElementById("first");
+const last = document.getElementById("last");
+const birth = document.getElementById("birthdate");
+const options = document.getElementById(
+  "location1",
+  "location2",
+  "location3",
+  "location4",
+  "location5",
+  "location6"
+);
+const cgv = document.getElementById("checkbox1");
 const submitBtn = document.querySelector(".btn-submit");
 const mondalForm = document.getElementById("modal-form");
 // launch modal event
@@ -29,27 +40,55 @@ const closeModal = () => {
   modalbg.style.display = "none";
 };
 
-//fonction valdier le form
-const validForm = () => {
-  submitBtn.style.cursor = "not-allowed";
-};
-
 //close modal
 closed.forEach((span) => span.addEventListener("click", closeModal));
 
 // fonction verifier form
 
-const validate = () => {
-  const errorBox = document.getElementById("first-error");
-  if (first.value.length < 2) {
-    errorBox.textContent = "prenom + 2 caract";
+const checkform = () => {
+  // first name check
+  const errorBoxFirst = document.getElementById("first-error");
+  if (first.value.length < 2 && last.value.length < 2) {
+    errorBoxFirst.textContent =
+      "Veuillez entrer 2 caractères ou plus pour le champ du nom.";
   } else {
-    errorBox.style.display = "none";
+    errorBoxFirst.style.display = "none";
+  }
+  //last name check
+  const errorBoxLast = document.getElementById("last-error");
+  if (last.value.length < 2) {
+    errorBoxLast.textContent =
+      "Veuillez entrer 2 caractères ou plus pour le champ du nom.";
+  } else {
+    errorBoxLast.style.display = "none";
+  }
+  //birth check
+  const errorBirth = document.getElementById("birth-error");
+  if (birth.value) {
+    errorBirth.style.display = "none";
+  } else {
+    errorBirth.textContent = "Vous devez entrer votre date de naissance.";
+  }
+  // options check
+  const errorOption = document.getElementById("option-error");
+  if (options.checked >= 1) {
+    errorOption.style.display = "none";
+  } else {
+    errorOption.textContent = "Vous devez choisir une option.";
+  }
+  // CGV check
+  const errorCgv = document.getElementById("cgv-error");
+  if (cgv.checked) {
+    errorCgv.style.display = "none";
+  } else {
+    errorCgv.textContent =
+      "Vous devez vérifier que vous acceptez les termes et conditions.";
   }
 };
 
 //valider le form
 mondalForm.addEventListener("submit", function (event) {
   event.preventDefault();
-  validate();
+  checkform();
+  alert("Merci ! Votre réservation a été reçue.");
 });
